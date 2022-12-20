3c617bbad18f462dbd1c667ac1ee52ddc2ac3679
joyahmed <joy_ahmed_007@yahoo.com>
2022-12-21 05:07:50 +0600

    ✅about

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,9 +1,62 @@
 'use client';
 
+import { TypingText } from '@/components';
+import { fadeIn, staggerContainer } from '@/utils/motion';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import Link from 'next/link';
+
 const About = () => (
-  <section>
-    About section
-  </section>
+	<section className='relative paddings z-10'>
+		<div className='gradient-02 z-0'></div>
+		<motion.div
+			varitants={staggerContainer}
+			initial='hidden'
+			whileInView='show'
+			viewport={{ once: false, amount: 0.25 }}
+			className='flex-center flex-col innerWidth mx-auto'
+		>
+			<TypingText
+				{...{
+					title: '| About Metaversus',
+					textStyles: 'text-center'
+				}}
+			/>
+
+			<motion.p
+				variants={fadeIn('up', 'tween', 0.2, 1)}
+				className='mt-[8px] font-normal text-[20px] sm:text-[32px] text-center text-secondary-white'
+			>
+				<span className='font-extrabold text-white'>Metaverse </span>
+				is a new thing in the future, where you can enjoy the virtual
+				world by feeling like it&apos;s really real, you can feel what
+				you feel in this metaverse world, because this is really the
+				<span className='font-extrabold text-white'>
+					{' '}
+					madness of the metaverse{' '}
+				</span>
+				of today, using only
+				<span className='font-extrabold text-white'> VR </span>
+				devices you can easily explore the metaverse world you want,
+				turn your dreams into reality. Let&apos;s{' '}
+				<span className='font-extrabold text-white'> explore </span>
+				the madness of the metaverse by scrolling down
+			</motion.p>
+		</motion.div>
+		<motion.div
+			variants={fadeIn('up', 'tween', 0.3, 1)}
+			initial='hidden'
+			whileInView='show'
+			className='flexCenter w-full mt-[28px]'
+		>
+			<Image
+				src='/arrow-down.svg'
+				alt='arrow down'
+				width={18}
+				height={28}
+			/>
+		</motion.div>
+	</section>
 );
 
 export default About;
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,9 +1,66 @@
 'use client';
 
+import {
+	slideIn,
+	staggerContainer,
+	textVariant
+} from '@/utils/motion';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import Link from 'next/link';
+
 const Hero = () => (
-  <section>
-    Hero section
-  </section>
+	<section className='yPaddings sm:pl-16 pl-6'>
+		<motion.div
+			variants={staggerContainer}
+			initial='hidden'
+			whileInView='show'
+			viewport={{ once: false, amount: 0.25 }}
+			className='flex flex-col innerWidth mx-auto'
+		>
+			<div className='relative flex flex-col items-center justify-center z-10'>
+				<motion.h1
+					variants={textVariant(1.1)}
+					className='heroHeading'
+				>
+					Metaverse
+				</motion.h1>
+				<motion.div
+					variants={textVariant(1.2)}
+					className='flex flex-row justify-center items-center'
+				>
+					<h1 className='heroHeading'>Ma</h1>
+					<div className='heroDText'></div>
+					<h1 className='heroHeading'>Ness</h1>
+				</motion.div>
+			</div>
+			<motion.div
+				variants={slideIn('right', 'tween', 0.2, 1)}
+				className='relative w-full -mt-[12px] md:-mt-[20px]'
+			>
+				<div className='relative w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]' />
+				<Image
+					src='/cover.png'
+					alt='cover'
+					fill
+					className='object-cover rounded-tl-[140px] z-10'
+				/>
+				<Link
+					href='#explore'
+					className='flex items-center justify-end w-[95%]'
+				>
+					<div className='relative flex w-[100px] h-[100px] sm:w-[155px] sm:h-[155px] -mt-[50px] sm:-mt-[70px] pr-[40px] z-10'>
+						<Image
+							src='/stamp.png'
+							alt='stamp'
+							fill
+							className='object-contain mt-16'
+						/>
+					</div>
+				</Link>
+			</motion.div>
+		</motion.div>
+	</section>
 );
 
 export default Hero;
